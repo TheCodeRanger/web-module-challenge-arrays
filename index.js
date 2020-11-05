01440de0d90d8e16e4978cc20a4a35c10ae34e7e
Christopher Greene <greenebasecamp@gmail.com>
2020-11-04 23:56:09 -0500

    test

--- index.js
+++ index.js
@@ -66,9 +66,9 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(array){
-    if 
-}
+// function is31Flavors(array){
+//     if 
+// }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
